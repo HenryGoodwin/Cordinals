d39ee217dce72ff277b10cf62b948d4f9bf000d7
Henry Goodwin <HenryGoodwin@Henrys-MacBook-Pro.local>
2016-01-23 17:02:17 +1000

    Conditionals 1st Commit

---
+++ Contents.swift
@@ -0,0 +1,69 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var ismyhouseonfire: Bool = false
+
+var anotherbool: Bool = true
+
+if ismyhouseonfire == true {
+    
+    print("Someone get me some water")
+    
+} else{
+    
+    print("someone get some fire for my house")
+    
+}
+
+var result = true ==  true
+result = true == false
+result = false == false
+
+var accountTotal = 30.33
+var item = 59.99
+
+if accountTotal >= item {
+    
+    print("I just got the item")
+
+} else {
+        
+        print("I dont have enough money")
+}
+
+var name = "Jack"
+if name <= "Jack" {
+    print("Your name is Jack")
+} else {
+    
+    print("Your name is not jack")
+    
+}
+
+var numberA = 25
+var numberB = 40
+var numberC = 100
+var numberD = 50
+
+var finalNumber = 100
+
+if numberA == finalNumber {
+    
+    print(numberA)
+    
+} else if numberB == finalNumber {
+    
+    print(numberB)
+    
+} else if numberC == finalNumber {
+    
+    print(numberC)
+    
+} else {
+    
+    print("None Of The Values Were Equal To The Final Number")
+    
+}
+
+
